a6bfeee11b52b0fc62bb9353b2aa825ff637c1f2
Thomas Dethorey <thomas.dethorey01@gmail.com>
2018-10-14 14:47:44 +0200

    ex 1

---
+++ EX1JSP1/assets/js/script.js
@@ -0,0 +1,4 @@
+// déclaration de la variable name initialisée avec une chaine de caractère (string)
+var name = "thomas";
+// affichage de ma variable name dans une boite de dialogue (pop-up) avec la fonction alert.
+  alert(name);
